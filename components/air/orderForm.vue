1719be1843ec4efcdecc254f5d6813cb3e472265
A-Lin <a_lin2019@126.com>
2019-08-06 14:39:07 +0800

    机票订单表单样式完成

--- components/air/orderForm.vue
+++ components/air/orderForm.vue
@@ -1,69 +1,64 @@
 <template>
-  <div class="contaniner">
+  <div class="container">
     <div class="passengerInfo">
       <h2>乘机人</h2>
-      <el-form ref="passengerform" :model="passengerform">
+      <el-form>
         <el-form-item label="乘机人类型">
-          <el-input placeholder="姓名" v-model="passengerform.name" class="input-with-select">
-            <el-select v-model="ageSelect" slot="prepend" placeholder="请选择">
+          <el-input placeholder="姓名" v-model="users[0].username" class="input-with-select">
+            <el-select value="1" slot="prepend" placeholder="请选择">
               <el-option label="成人" value="1"></el-option>
             </el-select>
           </el-input>
         </el-form-item>
         <el-form-item label="证件类型">
-          <el-input placeholder="证件号码" v-model="passengerform.card" class="input-with-select">
-            <el-select v-model="certSelect" slot="prepend" placeholder="请选择">
+          <el-input placeholder="证件号码" v-model="users[0].id" class="input-with-select">
+            <el-select value="1" slot="prepend" placeholder="请选择">
               <el-option label="身份证" value="1"></el-option>
               <el-option label="护照" value="2"></el-option>
             </el-select>
           </el-input>
         </el-form-item>
-        <el-form-item>
-          <el-button type="primary">添加乘机人</el-button>
-        </el-form-item>
+        <span class="delete-user">-</span>
       </el-form>
     </div>
+    <el-button type="primary" class="add-user">添加乘机人</el-button>
     <div class="insurance">
       <h2>保险</h2>
-      <el-checkbox-group v-model="checkList">
-        <el-checkbox label="航空意外险：￥30/份x1 最高赔付260万"></el-checkbox>
-        <br />
-
-        <el-checkbox label="航空延误险：￥30/份x1 最高赔付300万"></el-checkbox>
+      <el-checkbox-group v-model="insurances">
+        <el-checkbox label="航空意外险：￥30/份x1 最高赔付260万" border></el-checkbox>
       </el-checkbox-group>
     </div>
     <div class="linkman">
       <h2>联系人</h2>
-      <el-form ref="linkmanform" :model="linkmanform" label-width="80px">
+      <el-form label-width="80px">
         <el-form-item label="姓名">
-          <el-input v-model="linkmanfrom.name"></el-input>
+          <el-input v-model="contactName"></el-input>
         </el-form-item>
         <el-form-item label="手机">
-          <el-input v-model="linkmanfrom.phone"></el-input>
+          <el-input placeholder="请输入内容" v-model="contactPhnoe">
+            <template slot="append">发送验证码</template>
+          </el-input>
         </el-form-item>
         <el-form-item label="验证码">
-          <el-input v-model="linkmanfrom.code"></el-input>
+          <el-input v-model="captcha"></el-input>
         </el-form-item>
       </el-form>
     </div>
+    <el-button type="warning" class="btn">提交订单</el-button>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      passengerform: {
-        name: '',
-        card: ''
-      },
-      ageSelect: '',
-      certSelect: '',
-      checkList: '',
-      linkmanform: {
-        name: '',
-        phone: '',
-        code: ''
-      }
+      users: [{ username: '', id: '' }],
+      insurances: [{ id: '' }],
+      contactName: '',
+      contactPhnoe: '',
+      captcha: '',
+      invoice: false,
+      seat_xid: '',
+      air: 0
     }
   }
 
@@ -71,25 +66,60 @@ export default {
 </script>
 <style lang="less" scoped>
 .container {
-  margin-top: 20px;
+  margin: 20px 0;
+  padding-bottom: 30px;
+  border-bottom: 1px dashed #ddd;
   .passengerInfo {
-    padding: bottom 20px;
-    border-bottom: 1px dashed #ddd;
+  }
+  .delete-user {
+    display: block;
+    background: #ddd;
+    width: 16px;
+    height: 16px;
+    font-size: 14px;
+    text-align: center;
+    line-height: 16px;
+    color: #fff;
+    cursor: pointer;
+    border-radius: 50px;
+    position: absolute;
+    right: -30px;
+    top: 50%;
+  }
+  .add-user {
+    margin: 20px 0;
   }
   h2 {
     font-weight: normal;
     margin: 20px 0;
   }
   .insurance {
+    padding-bottom: 40px;
     border-bottom: 1px dashed #ddd;
+    border-top: 1px dashed #ddd;
   }
   /deep/.el-checkbox {
     width: 317px;
     height: 40px;
-    line-height: 40px;
+    line-height: 22px;
     text-align: center;
     margin-bottom: 20px;
     border: 1px solid #ddd;
+  }
+
+  .linkman {
+    /deep/ .el-input {
+      width: 300px;
+      height: 30px;
+    }
+  }
+
+  .btn {
+    width: 268px;
+    height: 58px;
+    text-align: center;
+    line-height: 37px;
+    margin-left: 200px;
   }
 }
 </style>
--- pages/air/order.vue
+++ pages/air/order.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container w">
     <el-row type="flex" justify="space-between">
       <el-col :span="14">
         <OrderForm />
