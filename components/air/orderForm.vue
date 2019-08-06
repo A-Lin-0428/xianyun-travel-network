26b098491323fff831c925459b8f8bcd8e7724c0
A-Lin <a_lin2019@126.com>
2019-08-06 16:00:18 +0800

    渲染保险数据出现length报错

--- components/air/orderForm.vue
+++ components/air/orderForm.vue
@@ -2,30 +2,34 @@
   <div class="container">
     <div class="passengerInfo">
       <h2>乘机人</h2>
-      <el-form>
+      <el-form v-for="(item,index) in users" :key="index" style="position: relative;">
         <el-form-item label="乘机人类型">
-          <el-input placeholder="姓名" v-model="users[0].username" class="input-with-select">
+          <el-input placeholder="姓名" v-model="item.username" class="input-with-select">
             <el-select value="1" slot="prepend" placeholder="请选择">
               <el-option label="成人" value="1"></el-option>
             </el-select>
           </el-input>
         </el-form-item>
         <el-form-item label="证件类型">
-          <el-input placeholder="证件号码" v-model="users[0].id" class="input-with-select">
+          <el-input placeholder="证件号码" v-model="item.id" class="input-with-select">
             <el-select value="1" slot="prepend" placeholder="请选择">
               <el-option label="身份证" value="1"></el-option>
               <el-option label="护照" value="2"></el-option>
             </el-select>
           </el-input>
         </el-form-item>
-        <span class="delete-user">-</span>
+        <span class="delete-user" v-show="index!=0" @click="handleDel(index)">-</span>
       </el-form>
     </div>
-    <el-button type="primary" class="add-user">添加乘机人</el-button>
+    <el-button type="primary" class="add-user" @click="handleAddUser">添加乘机人</el-button>
     <div class="insurance">
       <h2>保险</h2>
-      <el-checkbox-group v-model="insurances">
-        <el-checkbox label="航空意外险：￥30/份x1 最高赔付260万" border></el-checkbox>
+      <el-checkbox-group v-for="(item,index) in data.insurances&&data.insurances" :key="index">
+        <el-checkbox
+          :label="`${item.type}：￥${item.price}/份x1 最高赔付${item.compensation}`"
+          border
+          @change="handleInsuId(item)"
+        ></el-checkbox>
       </el-checkbox-group>
     </div>
     <div class="linkman">
@@ -49,17 +53,42 @@
 </template>
 <script>
 export default {
+  props: {
+    data: {
+      type: Object,
+      default: {}
+    }
+  },
   data() {
     return {
       users: [{ username: '', id: '' }],
-      insurances: [{ id: '' }],
+      insurances: [],
       contactName: '',
       contactPhnoe: '',
       captcha: '',
       invoice: false,
       seat_xid: '',
-      air: 0
+      air: 0,
+
     }
+  },
+  methods: {
+    // 添加乘机人
+    handleAddUser() {
+      // 给users添加一个对象
+      this.users.push({ username: '', id: '' })
+    },
+    // 点击删除乘机人
+    handleDel(index) {
+      this.users.splice(index, 1)
+    },
+    // 点击保险选项的触发
+    handleInsuId(item) {
+      console.log(item)
+    }
+  },
+  mounted() {
+    console.log(this.data)
   }
 
 }
@@ -69,8 +98,6 @@ export default {
   margin: 20px 0;
   padding-bottom: 30px;
   border-bottom: 1px dashed #ddd;
-  .passengerInfo {
-  }
   .delete-user {
     display: block;
     background: #ddd;
@@ -83,6 +110,7 @@ export default {
     cursor: pointer;
     border-radius: 50px;
     position: absolute;
+
     right: -30px;
     top: 50%;
   }
--- pages/air/order.vue
+++ pages/air/order.vue
@@ -2,7 +2,7 @@
   <div class="container w">
     <el-row type="flex" justify="space-between">
       <el-col :span="14">
-        <OrderForm />
+        <OrderForm :data="infoData.insurances" />
       </el-col>
       <el-col :span="10"></el-col>
     </el-row>
@@ -13,8 +13,34 @@
 // 引用组件
 import OrderForm from '@/components/air/orderForm'
 export default {
+  data() {
+    return {
+      infoData: {
+        insurances: []
+      }
+    }
+  },
   components: {
     OrderForm
+  },
+  methods: {
+    getData() {
+      const { id, seat_xid } = this.$route.query
+      // 获取保险列表信息
+      this.$axios({
+        url: '/airs/' + id,
+        params: { seat_xid, }
+      }).then(res => {
+        // console.log(res)
+        this.infoData.insurances = res.data.insurances
+        console.log(this.infoData)
+      })
+    }
+  },
+  mounted() {
+    this.getData()
+    // 先获取到发送请求的id和seat_xid的值
+
   }
 }
 </script>
