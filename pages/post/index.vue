9467c0410d10ca44b89acd4930d2f64c3908ec6b
saki <39420706@qq.com>
2019-08-07 16:32:22 +0800

    carmen测试提交

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,12 +1,38 @@
 <template>
-  <div>旅游业务首页</div>
+  <div class="container">
+    <el-row type="flex" justify="space-between">
+      <!-- 侧栏 -->
+      <div class="main"></div>
+
+      <!-- 攻略列表 -->
+      <div class="aside">
+        <postList />
+      </div>
+    </el-row>
+  </div>
 </template>
 
 <script>
-export default {
+import postList from "@/components/post/postList";
 
-}
+export default {
+  components: {
+    postList
+  }
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  width: 1000px;
+  margin: 20px auto;
+}
+
+/*aside*/
+.aside {
+  width: 700px;
+  height: fit-content;
+  // border: 1px #ddd solid;
+  // background: #333;
+}
 </style>
