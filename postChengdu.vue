af937dfe7f77fd72eae067ae6a4ff5cf26369aa5
liqihangpanghu <52402136+liqihangpanghu@users.noreply.github.com>
2019-08-10 15:56:57 +0800

    旅游详情组件

---
+++ postChengdu.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="aa">
+    <div class="strategy-left">
+      <span>旅游攻略</span>/ 攻略详情
+      <h2>这里是成都！一座来了就不想走的城市～</h2>
+      <hr />
+      <div class="Strategy-of-time">
+        <p>攻略：2019-05-22 10:57 阅读：5691</p>
+      </div>
+      <div class="Strategy-content">
+        <p>时间：2019年4月30日～2019年5月4日</p>
+        <p>人物：我们一家三口</p>
+        <p>地点： 成都</p>
+        <p>设备：7M2+FE55F1.8</p>
+        <p>行李：一个28寸行李箱</p>
+        <p>熊娃现在是小学二年级下学期了，今年她听完了凯叔三国演义，最喜欢刘备的蜀国，会嫌弃阿斗太笨是被摔傻了……</p>
+        <p>于是在五一假期从1天变成4天的之后，我订了来回 成都 的机票，决定带她来看一看刘备的蜀国～</p>
+        <p>四川 很大，可以玩的地方很多，但是只有4天，开始还安排了去 青城山 和 都江堰 ，后面决定还是市区走走吃吃就好了～</p>
+        <p>出发前行程</p>
+
+        <img
+          src="http://n4-q.mafengwo.net/s13/M00/B7/94/wKgEaVzLgBeAAEP6AAQP-fApUus99.jpeg?imageMogr2%2Fthumbnail%2F1360x%2Fstrip%2Fquality%2F90"
+        />
+        <p>行前准备：</p>
+
+        <p>1、机票</p>
+
+        <p>机票是深航里程兑换，没太多可以说的。</p>
+
+        <p>2、酒店</p>
+
+        <p>成都 的酒店很多，各种价位总能找到你心水的。我们这次主要游玩的是市区，除了最远的大熊猫基地要十几公里外，其他都集中在二环以内。我们出行主要的交通工具是滴滴，所以考虑要找个在这些比较中心的地方住会比较方便。一开始想住在春熙路，方便逛，看了好几家网红酒店。后面我哥跟我说飞猪F2每年有两次住宿权益让我关注下，那天正好周四，明宇还有五折，最后就选了明宇旗下的尚雅酒店订了下来，酒店位置很好，距离各大景区都在三四公里内，订下来尚雅客房才354一晚，性价比超高！</p>
+
+        <p>不过飞猪只能定两晚，剩下的一晚也是支付宝活动，缤客288返现88，订了一晚最低价的旅行者客房426.87，再返现88，起来是338.87，然后入住的时候不出意外滴升级到和前两晚一样的房型了，省得换房！住下来感觉性价比超高的！</p>
+
+        <p>最后一晚，因为4号是早上八点多的飞机，我们不愿意早起，于是就跑机场旁边去住了，也是三百多～</p>
+
+        <p>3、门票</p>
+
+        <p>成都 的景点门票都不贵，线上线下也都很方便，而且几乎没便宜多少，有的还会更贵， 比如 武侯祠门票50，线上卖到了五十几，还要门口排队取票，我果断退款了线上票，门口直接买了，还快了不少。更有大熊猫繁育研究基地是打车路上提前买的，结果后面大门口的时候，电子票爆满，实体票 通道 一个人都没有，我内心是崩溃的～</p>
+
+        <p>所以真心不着急线上买，到时候为了省线上几块钱，多排半个小时您觉着划算吗？</p>
+
+        <p>唯一行前定下的就是芙蓉国粹的票，飞猪上用里程抵扣后3张甲等票509.99，位置超好！这个可以有！</p>
+
+        <p>PS：不过线不线上买还是具体看地方，杜甫草堂的时候，线上还是有优势的～只能说具体看了</p>
+
+        <p>4、保险</p>
+
+        <p>依然是买个保险傍身，只是国内游没太多要求，随意买就行了～</p>
+
+        <p>5、主要用用到的APP</p>
+
+        <p>飞猪、携程、大众点评、百度地图、滴滴</p>
+
+        <p>PS：我们比较懒，懒的走，地铁虽然方便但是有一种叫走死在到地铁站的路上，所以一般国内城市出行我都选尽量近的住宿靠滴滴，实在需要地铁的城市选择住地铁站附近～这次团了两个滴滴五一的套餐，正好覆盖了五一出行的四天</p>
+        <div class="Content-of-a">
+          <h2>第一天 4.30 我们出发啦！宽窄巷子</h2>
+          <img
+            src="http://b3-q.mafengwo.net/s13/M00/FF/47/wKgEaVzMTOCAHX9_AAnidFWU9K466.jpeg?imageMogr2%2Fthumbnail%2F1360x%2Fstrip%2Fquality%2F90"
+          />
+          <span>泉州晋江机场</span>
+        </div>
+        <div class="Content-of-b">
+          <p>我们是下午一点半的飞机，于是给熊娃请了假一放学就赶飞机去了。从 泉州 到 成都 要三个小时，所以到 成都 已然饭点了。</p>
+
+          <p>本来想好了第一餐是想吃 成都 吃客的，于是下了飞机就在手机上排了号，结果30那天大堵车平时二十分钟能到的酒店活生生走了四五十分钟，而排号也在我们快到酒店前过号了。</p>
+
+          <p>酒店位置不错，除了宽窄巷子，其他景点都在二十分钟车程以内，这样方便我们回酒店午休～</p>
+
+          <p>带孩子出门吃饭和睡觉是最重要的</p>
+          <img
+            src="http://p4-q.mafengwo.net/s13/M00/02/39/wKgEaVzMTgKALFi5AAaKsyezLBE34.jpeg?imageMogr2%2Fthumbnail%2F1360x%2Fstrip%2Fquality%2F90"
+          />
+
+          <span>成都明宇雅阁饭店</span>
+          <p>在酒店放下行李，就准备到宽窄巷子吃饭，结果很尴尬的事发生了，竟然叫不到车。最后只能门口扫了两辆摩拜骑过去了，还好也不远，骑车宽窄巷子二十几分钟。</p>
+          <img
+            src="http://n2-q.mafengwo.net/s13/M00/02/3E/wKgEaVzMTgOAeSLFAAha5pnV_4A12.jpeg?imageMogr2%2Fthumbnail%2F1360x%2Fstrip%2Fquality%2F90"
+          />
+          <span>宽窄巷子景区</span>
+          <p>宽窄巷子分为宽巷子（巷子比较宽），窄巷子（巷子比较窄），井巷子（巷子里有口井），是清朝旗人的居住地，所以和 北京 的巷子文化有异曲同工之妙。现在的宽窄巷子多为翻建的建筑，里面遍布了不少有意思的店铺，如果人不多，不赶时间可以好好逛逛。我们主要走了窄巷子，人潮那个汹涌，所以没有时间慢慢逛。宽巷子和井巷子也稍微逛了一下，好像还是窄巷子人最多.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="less" scoped>
+.aa {
+  width: 1000px;
+  margin: 0 auto;
+}
+// 攻略左边开始
+.strategy-left {
+  width: 700px;
+  margin-right: 0;
+}
+
+.strategy-left span {
+  margin-bottom: 50px;
+  color: #22609c;
+}
+.strategy-left h2 {
+  font-size: 32px;
+  margin-bottom: 30px;
+  margin-top: 30px;
+}
+
+.Strategy-of-time {
+  height: 60px;
+  p {
+    color: #989894;
+    float: right;
+    margin-top: 15px;
+  }
+}
+.Strategy-content img {
+  margin-top: 20px;
+  border-bottom: 15px solid #fbf1d8;
+}
+.Strategy-content p {
+  font-size: 15px;
+}
+
+.Strategy-content img {
+  width: 700px;
+  height: 599px;
+}
+.Strategy-content .zhuxi {
+  color: #989894;
+  margin-bottom: 20px;
+}
+.Content-of-a h2 {
+  font-size: 22px;
+}
+.Content-of-a img {
+  width: 700px;
+  height: 467px;
+  margin-top: 20px;
+  border-bottom: 15px solid #fbf1d8;
+}
+.Content-of-b img {
+  width: 700px;
+  height: 467px;
+}
+.Content-of-b p {
+  font-size: 16px;
+}
+.Content-of-b .img-size {
+  margin-bottom: 30px;
+}
+.Content-of-b .img-size img {
+  width: 700px;
+  height: 545px;
+  border-bottom: 15px solid #fbf1d8;
+}
+.strategy-comment {
+  margin: 0 auto;
+}
+// 攻略左边结束
+// 右边开始
+.strategy-right {
+  float: right;
+  width: 300px;
+}
+.strategy-like {
+  color: #ffa500;
+  margin-top: 50px;
+  margin-bottom: 30px;
+  i {
+    font-size: 30px;
+  }
+}
+.ctrl-item {
+  margin-top: 50px;
+  margin: 0 auto;
+}
+</style>
+
+
